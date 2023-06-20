8a587c1f0d18c1dde398a95322fad060f8618f1a
shizhong <shizhong1989@gmail.com>
2023-06-19 22:41:06 -0700

    Add slider

--- wlJourneys.js
+++ wlJourneys.js
@@ -12,3 +12,45 @@ window.onscroll = () => {
     scrollDiv.style.top = "0px";
   }
 };
+
+// slider
+var left = document.getElementById("slider-left");
+var right = document.getElementById("slider-right");
+var inner = document.querySelector(".slider-inner");
+var firstImg = document.querySelector(".first-img");
+var cloneImg = firstImg.cloneNode();
+inner.appendChild(cloneImg);
+var i = 1;
+right.onclick = () => {
+  i++;
+  inner.style.transition = "0.5s";
+  inner.style.left = `${-i * 800}px`;
+  if (i == 4) {
+    setTimeout(() => {
+      i = 0;
+      inner.style.left = `${-i * 800}px`;
+      inner.style.transition = "none";
+    }, 500);
+  }
+};
+
+left.onclick = () => {
+  i--;
+  if (i == -1) {
+    i = 4;
+    inner.style.transition = "none";
+    inner.style.left = `${-i * 800}px`;
+    setTimeout(() => {
+      i--;
+      inner.style.transition = "0.5s";
+      inner.style.left = `${-i * 800}px`;
+    }, 0);
+    // 如果43-47注释掉，直接换成下面几行为什么不行呢？
+    // i--;
+    // inner.style.transition = "0.5s";
+    // inner.style.left = `${-i * 800}px`;
+  } else {
+    inner.style.transition = "0.5s";
+    inner.style.left = `${-i * 800}px`;
+  }
+};
