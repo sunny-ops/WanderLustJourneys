ce7fb396fc062bb264f1d83faf5c04990874c5f4
Shi Zhong <shizhong@yuans-air.lan>
2023-06-23 14:45:46 -0700

    membership grace button finished

--- JS/wlJourneys.js
+++ JS/wlJourneys.js
@@ -2,9 +2,9 @@
 var openEl = document.getElementsByClassName("open")[0];
 var topEl = document.getElementsByClassName("top")[0];
 var bottomEl = document.getElementsByClassName("bottom")[0];
-console.log(openEl);
-console.log(topEl);
-console.log(bottomEl);
+// console.log(openEl);
+// console.log(topEl);
+// console.log(bottomEl);
 window.onload = () => {
   topEl.style.top = `${-window.innerHeight * 0.5}px`;
   bottomEl.style.bottom = `${-window.innerHeight * 0.5}px`;
@@ -29,4 +29,29 @@ window.onscroll = () => {
   }
 };
 
+// membership section
+var verticalImgs = document.getElementsByClassName("vacation-image");
+var cardLocs = document.getElementsByClassName("card-location")
+var cardPrices = document.getElementsByClassName("card-price");
+var membershipDots = document.getElementsByClassName("membership-dot");
+var membershipOutlineCircle = document.getElementsByClassName("membership-outline-circle")
+var egyptCircle = document.getElementById("egypt-circle");
+var graceCircle = document.getElementById("grace-circle");
+var indonesiaCircle = document.getElementById("indonesia-circle");
+var franceCircle = document.getElementById("france-circle");
+graceCircle.onclick = () => {
+  console.log(membershipDots);
+  verticalImgs[0].setAttribute("src", "https://assets.website-files.com/606764630d23c37bf9d41bb1/607dcee44ed8fe667ad63e34_013.jpeg")
+  membershipDots[1].style.backgroundColor = "rgb(8, 28, 58)";
+  membershipOutlineCircle[1].style.display = "block";
+  for (let i = 0; i < 4; i++) {
+    if (i != 1) {
+      membershipDots[i].style.backgroundColor = "rgb(230, 230, 243)";
+      membershipOutlineCircle[i].style.display = "none";
+    } else {
+      membershipDots[1].style.backgroundColor = "rgb(8, 28, 58)";
+      membershipOutlineCircle[1].style.display = "block";
+    }
+  }
 
+}
