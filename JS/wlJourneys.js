e4d815627604fe649c56fefc9dd4825f7e41374b
Shi Zhong <shizhong@yuans-air.lan>
2023-06-23 12:52:31 -0700

    testimonial swiper done

--- JS/wlJourneys.js
+++ JS/wlJourneys.js
@@ -29,48 +29,4 @@ window.onscroll = () => {
   }
 };
 
-// slider
-var left = document.getElementById("slider-left");
-var right = document.getElementById("slider-right");
-var inner = document.querySelector(".slider-inner");
-var firstImg = document.querySelector(".first-img");
-var cloneImg = firstImg.cloneNode();
-inner.appendChild(cloneImg);
-var i = 1;
-right.onclick = () => {
-  console.log("right");
-  i++;
-  inner.style.transition = "0.5s";
-  inner.style.left = `${-i * 800}px`;
-  if (i == 4) {
-    setTimeout(() => {
-      i = 0;
-      inner.style.left = `${-i * 800}px`;
-      inner.style.transition = "none";
-    }, 500);
-  }
-};
-// console.log(left);
-// console.log(right);
-left.onclick = () => {
-  console.log("left");
-  i--;
-  if (i == -1) {
-    i = 4;
-    inner.style.transition = "none";
-    inner.style.left = `${-i * 800}px`;
-    setTimeout(() => {
-      i--;
-      inner.style.transition = "0.5s";
-      inner.style.left = `${-i * 800}px`;
-    }, 0);
-    // Q2. 如果43-47注释掉，直接换成下面几行为什么不行呢？
-    // i--;
-    // inner.style.transition = "0.5s";
-    // inner.style.left = `${-i * 800}px`;
-  } else {
-    inner.style.transition = "0.5s";
-    inner.style.left = `${-i * 800}px`;
-  }
-};
-// Q1. 第一次点右键滑动的时候没有延迟
+
